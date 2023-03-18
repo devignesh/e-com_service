fe54c4fb493a5b4b56fc8051bd32397dc049e82c
devignesh <vinesh1865@gmail.com>
2023-03-18 15:42:22 +0530

    initial commit with intial setup

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	routes "e-com/src"
+	"e-com/utils/database"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
+	r := gin.New()
+
+	setupRoutes(r)
+	setupDatabase()
+	startServer(port, r)
+}
+
+func setupRoutes(r *gin.Engine) {
+	routes.ProductRoutes(r)
+}
+
+func setupDatabase() {
+	//sets up database
+	database.Db()
+
+}
+
+// startServer to start the server
+func startServer(port string, r *gin.Engine) {
+	srv := &http.Server{
+		Addr:    port,
+		Handler: r,
+	}
+
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		fmt.Printf("Listen: %s\n", err)
+	}
+
+}
